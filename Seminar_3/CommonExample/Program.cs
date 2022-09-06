e316617add962c6a78d507acd1ef947fea200f74
m-scvorcov <m-scvorcov@it-serv.ru>
2022-09-06 21:05:07 +0300

    Добавлено решение общего задания

--- Seminar_3/CommonExample/Program.cs
+++ Seminar_3/CommonExample/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Определение четверти точки
+int GetPointArea(int x, int y) 
+{
+    if (x > 0 && y > 0) {
+        return 1;
+    }
+
+    if (x < 0 && y > 0) {
+        return 2;
+    }
+
+    if (x < 0 && y < 0) {
+        return 3;
+    }
+
+    if (x > 0 && y < 0) {
+        return 4;
+    }
+
+    return 0;
+}
+
+Console.Write("Input X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Inpot Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+int areaNum = GetPointArea(x, y);
+Console.WriteLine($"Area = {areaNum}");
+
