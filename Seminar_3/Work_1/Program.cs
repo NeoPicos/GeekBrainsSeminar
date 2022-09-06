00e4794c927104355d007e2281b4ef332715f83b
m-scvorcov <m-scvorcov@it-serv.ru>
2022-09-06 21:29:57 +0300

    Решение первой задачи

--- Seminar_3/Work_1/Program.cs
+++ Seminar_3/Work_1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string GetAreaRange(int areaNum) {
+    if (areaNum == 1) {
+        return "x>0 y>0";
+    }
+
+    if (areaNum == 2) {
+        return "x<0 y>0";
+    }
+
+    if (areaNum == 3) {
+        return "x<0 y<0";
+    }
+
+    if (areaNum == 4) {
+        return "x>0 y<0";
+    }
+
+    return "Введенный номер четверти больше 4-х";
+}
+
+Console.Write("Input area: ");
+int areaNum = Convert.ToInt32(Console.ReadLine());
+
+string result = GetAreaRange(areaNum);
+Console.WriteLine(result);
+
+
